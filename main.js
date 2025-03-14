7d88e2028d73756fad0fbb0bd247a038db227581
ArnavAggarwal22 <arnavaggarwal3095@gmail.com>
2025-03-15 02:22:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const scrollRevealOption = {
   duration: 1000,
 };
 
-// header container
+
 ScrollReveal().reveal(".header__container .section__subheader", {
   ...scrollRevealOption,
 });
@@ -35,19 +35,19 @@ ScrollReveal().reveal(".header__container .btn", {
   delay: 1000,
 });
 
-// room container
+
 ScrollReveal().reveal(".room__card", {
   ...scrollRevealOption,
   interval: 500,
 });
 
-// feature container
+
 ScrollReveal().reveal(".feature__card", {
   ...scrollRevealOption,
   interval: 500,
 });
 
-// news container
+ 
 ScrollReveal().reveal(".news__card", {
   ...scrollRevealOption,
   interval: 500,
